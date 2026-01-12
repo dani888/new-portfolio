0cdca83bc8c172e3f45a4cd7946706407becd162
dani888 <daniel.werminghausen@gmail.com>
2026-01-12 02:14:40 -0500

    wip:6 update contact me page and home page mobile

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,19 +1,13 @@
-// import Emailform from "./Emailform";
-
 import pdf from "../../public/images/Resume.pdf";
 import pdf2 from "../../public/images/Certification.pdf";
-// import linkin from "../../public/images/Linkedin.png";
-// import resum from "../../public/images/resume.io.png";
-// import git from "../../public/images/Git_logo.png";
-// import gmail from "../../public/images/Gmail_logo.png";
-// import github from "../../public/images/Github_logo.png";
 
-const Resume = (props) => {
+const Resume = () => {
   return (
     <div className="resume">
       <br />
       <br />
       <div className="flex">
+        {/* Intro text */}
         <div className="textbox">
           <h3 id="h1resume"><b>DANIEL RESUME & CONTACT</b></h3>
           <div className="column">
@@ -31,9 +25,11 @@ const Resume = (props) => {
         <br />
         <br />
 
+        {/* Desktop iframe */}
         <iframe
           title="Software_Engineer"
           src={pdf}
+          className="pdf-desktop"
           width="70%"
           height="1200px"
         ></iframe>
@@ -45,148 +41,23 @@ const Resume = (props) => {
         <iframe
           title="GA_Certification"
           src={pdf2}
+          className="pdf-desktop"
           width="70%"
           height="850px"
         ></iframe>
-        <br />
-        <br />
 
-        {/* <div className="w3-container w3-center w3-animate-right resumec">
-          <a
-            href="https:/https://resume.io/r/MMzUgbn97/r/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="imgresume"
-              alt="Resume.io"
-              src={resum}
-              width="160"
-              height="160"
-            />
+        {/* Mobile buttons */}
+        <div className="pdf-mobile">
+          <a href={pdf} target="_blank" rel="noopener noreferrer">
+            📄 View Resume (PDF)
           </a>
-          <hr className="lined2 imgresume" />
-
-          <a
-            href="https://www.linkedin.com/in/daniel-werminghausen32112333//"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="imgresume"
-              alt="LinkedIn"
-              src={linkin}
-              width="130"
-              height="130"
-            />
-          </a>
-          <hr className="lined2 imgresume" />
-
-          <a
-            href="https://github.com/dani888ni888?tab=overview&from=2021-11-01&to=2021-11-09"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              className="imgresume github"
-              alt="GitHub"
-              src={github}
-              width="160"
-              height="100"
-            />
-          </a>
-          <hr className="lined2 imgresume" />
-
-          <a href="mailto:Daniel.werminghausen@gmail.com" target="_blank" rel="noopener noreferrer">
-            <img
-              className="imgresume"
-              alt="Email"
-              src={gmail}
-              width="130"
-              height="80"
-            />
+          <br />
+          <a href={pdf2} target="_blank" rel="noopener noreferrer">
+            📄 View Certification (PDF)
           </a>
         </div>
 
         <br />
-
-        <div className="textbox">
-          <div className="column">
-            <div className="col s12 m6">
-              <div className="card">
-                <div className="flexbuttons">
-                  <a
-                    href="https://www.linkedin.com/in/daniel-werminghausen32112333/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img
-                      className="imgresume2"
-                      alt="LinkedIn"
-                      src={linkin}
-                      width="75"
-                      height="75"
-                    />
-                  </a>
-
-                  <a
-                    href="https:/https://resume.io/r/MMzUgbn97/r/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img
-                      className="imgresume2"
-                      alt="Resume.io"
-                      src={resum}
-                      width="80"
-                      height="80"
-                    />
-                  </a>
-
-                  <a
-                    href="https://github.com?tab=overview&from=2021-11-01&to=2021-11-09"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <img
-                      className="imgresume2"
-                      alt="GitHub"
-                      src={git}
-                      width="70"
-                      height="70"
-                    />
-                  </a>
-
-                  <a href="mailto:Daniel.werminghausen@gmail.com" target="_blank" rel="noopener noreferrer">
-                    <img
-                      className="imgresume2"
-                      alt="Email"
-                      src={gmail}
-                      width="110"
-                      height="75"
-                    />
-                  </a>
-
-                  <b className="mobilemail">
-                    <br />
-                    Email:
-                    <a href="mailto:Daniel.werminghausen@gmail.com"> Daniel.werminghausen@gmail.com </a>
-                  </b>
-                </div>
-
-                <b className="mobilemail2">
-                  <br />
-                  Email:
-                  <a href="mailto:Daniel.werminghausen@gmail.com"> Daniel.werminghausen@gmail.com </a>
-                </b>
-                <br />
-                Send me a Message:
-                <Emailform />
-              </div>
-            </div>
-          </div>
-        </div> */}
-
         <br />
       </div>
     </div>
